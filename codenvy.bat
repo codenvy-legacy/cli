466479c89c7a6cd8e8471126b195af3a8d9a7f36
Tyler Jewell <tjewell@codenvy.com>
2014-01-25 06:45:48 -0800

    scaffolding for service provider

--- codenvy.bat
+++ codenvy.bat
@@ -1,6 +1,5 @@
 @echo off
-SET JAVA_LOCATION="c:\Program Files\Java\jdk1.7.0_07\"
-SET JRE_HOME="c:\Program Files\Java\jdk1.7.0_07"
+SET JAVA_LOCATION="%JAVA_HOME%"
+SET JRE_HOME=%JAVA_HOME%
 cmd /c "%JAVA_LOCATION%\bin\java -jar target\codenvy-cli-1.0-SNAPSHOT.jar %*"
-REM %JAVA_LOCATION%\bin\java -jar target\codenvy-cli-1.0-SNAPSHOT.jar %*
 REM exit /b %errorlevel%
