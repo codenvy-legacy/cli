93c106b4c9385e09e86160b6ae3c2608b31346ab
Tyler Jewell <tjewell@codenvy.com>
2014-01-29 17:18:15 -0800

    Clean up

--- codenvy_through_proxy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-SET JAVA_LOCATION="c:\Program Files\Java\jdk1.7.0_07\"
-SET JRE_HOME="c:\Program Files\Java\jdk1.7.0_07"
-cmd /c "%JAVA_LOCATION%\bin\java -DproxySet=true -DproxyHost=127.0.0.1 -DproxyPort=8888 -Djavax.net.ssl.trustStore=c:\codenvy\FiddlerKeystore -Djavax.net.ssl.trustStorePassword=grouch -Djava.util.logging.config.file=logging.properties -jar target\codenvy-cli-1.0-SNAPSHOT.jar %*"
-exit /b %errorlevel%
