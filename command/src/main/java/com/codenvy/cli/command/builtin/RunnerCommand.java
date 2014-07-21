3e86fddde58ed80d5f471860fe3ec7df4b4f3d73
Florent Benoit <Florent.Benoit@serli.com>
2014-07-21 08:57:03 +0200

    Fixes #15 : run command should only return process ID and not wait for now

--- command/src/main/java/com/codenvy/cli/command/builtin/RunnerCommand.java
+++ command/src/main/java/com/codenvy/cli/command/builtin/RunnerCommand.java
@@ -10,106 +10,96 @@
  *******************************************************************************/
 package com.codenvy.cli.command.builtin;
 
+import com.codenvy.cli.command.builtin.model.DefaultUserRunnerStatus;
+import com.codenvy.cli.command.builtin.model.UserProject;
+import com.codenvy.cli.command.builtin.model.UserRunnerStatus;
 import com.codenvy.client.Codenvy;
 import com.codenvy.client.model.Project;
 import com.codenvy.client.model.RunnerStatus;
 import com.codenvy.client.model.Workspace;
 import com.codenvy.client.model.WorkspaceRef;
 
+import org.apache.karaf.shell.commands.Argument;
 import org.apache.karaf.shell.commands.Command;
 import org.apache.karaf.shell.commands.Option;
+import org.fusesource.jansi.Ansi;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import static com.codenvy.client.model.RunnerState.STOPPED;
+import static org.fusesource.jansi.Ansi.Attribute.INTENSITY_BOLD;
+import static org.fusesource.jansi.Ansi.Attribute.INTENSITY_BOLD_OFF;
 
 /**
  * Allows to run a given project
  * @author Florent Benoit
  */
 @Command(scope = "codenvy", name = "run", description = "Run a project in Codenvy System")
-public class RunnerCommand extends AbsCommand {
+public class RunnerCommand extends ScopedIDCommand {
 
-    @Option(name = "-w", aliases = {"--workspace"}, description = "Specify the workspace name", required = false, multiValued = false)
-    private String workspaceName;
+    /**
+     * Executor service
+     */
+    //private ExecutorService executorService;
 
-    @Option(name = "-p", aliases = {"--project"}, description = "Specify the project name", required = false, multiValued = false)
-    private String projectName;
+    /**
+     * Default constructor.
+     */
+    public RunnerCommand() {
+       // this.executorService = Executors.newFixedThreadPool(10);
+    }
+
 
     @Override
     protected Object doExecute() throws Exception {
         final Codenvy current = checkLoggedIn();
+
+        // not logged in
         if (current == null) {
             return null;
         }
 
-        WorkspaceRef workspaceRef = null;
-
-        Workspace workspace = getCurrentWorkspace();
-        if (workspace != null) {
-            workspaceRef = workspace.workspaceRef();
-        }
-        if (workspaceRef == null) {
-            if (workspaceName == null) {
-                session.getConsole().println("No current workspace is selected. Please select a workspace.");
-                return null;
-            } else {
-                // select this workspace
-                workspaceRef = current.workspace().withName(workspaceName).execute();
-            }
+        // do we have the projectID ?
+        String projectShortId = getScopedProjectId();
+        if (projectShortId == null) {
+            Ansi buffer = Ansi.ansi();
+            buffer.fg(Ansi.Color.RED);
+            buffer.a("No projectID has been set");
+            buffer.fg(Ansi.Color.DEFAULT);
+            session.getConsole().println(buffer.toString());
+            return null;
         }
 
-        // get workspace ID
-        String workspaceID = workspaceRef.id();
-        session.getConsole().println("Using workspace ID" + workspaceID);
+        // get project for the given shortID
+        UserProject project = getProject(current, projectShortId);
 
-        Project project = getCurrentProject();
         if (project == null) {
-            if (projectName == null) {
-                session.getConsole().println("No current project is selected. Please select a project.");
-                return null;
-            } else {
-                // select a project
-                List<? extends Project> projects = current.project().getWorkspaceProjects(workspaceID).execute();
-                for (Project foundProject : projects) {
-                    if (foundProject.name().equals(projectName)) {
-                        project = foundProject;
-                        break;
-                    }
-                }
-            }
+            Ansi buffer = Ansi.ansi();
+            buffer.fg(Ansi.Color.RED);
+            buffer.a("No matching project for identifier '").a(projectShortId).a("'.");
+            buffer.fg(Ansi.Color.DEFAULT);
+            session.getConsole().println(buffer.toString());
+            return null;
         }
 
-        final Project projectToRun = project;
+
+        final Project projectToRun = project.getInnerProject();
 
         // Ok now we have the project
-        final RunnerStatus runnerStatus = current.runner().run(project).execute();
+        final RunnerStatus runnerStatus = current.runner().run(projectToRun).execute();
 
-        new Runnable() {
+        UserRunnerStatus userRunnerStatus = new DefaultUserRunnerStatus(runnerStatus, project);
 
-            @Override
-            public void run() {
-                RunnerStatus updatedStatus = runnerStatus;
-                while (updatedStatus.status() != STOPPED) {
-                    updatedStatus = current.runner().status(projectToRun, runnerStatus.processId()).execute();
-
-                    String log = null;
-                    try {
-                        log = current.runner().logs(projectToRun, runnerStatus.processId()).execute();
-                    } catch (RuntimeException e) {
-                        session.getConsole().println(e);
-                    }
-                    if (log != null && log.length() > 0) {
-                        session.getConsole().println("Found log = " + log);
-                    }
-                    try {
-                        Thread.sleep(1000L);
-                    } catch (InterruptedException e) {
-                        session.getConsole().println(e);
-                    }
-                }
-            }
-        }.run();
+        Ansi buffer = Ansi.ansi();
+        buffer.a("Run task for project ").a(INTENSITY_BOLD).a(project.name()).a(INTENSITY_BOLD_OFF).a("' has been submitted with process ID ").a(INTENSITY_BOLD).a(userRunnerStatus.shortId()).a(INTENSITY_BOLD_OFF);
+        session.getConsole().println(buffer.toString());
 
         return null;
     }
