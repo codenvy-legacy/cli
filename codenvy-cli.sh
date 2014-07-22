046cb7548eda4704203c5489b977ce21a6015276
Florent Benoit <Florent.Benoit@serli.com>
2014-07-22 09:00:19 +0200

    Fix #32 : Provides shortcut to launch the CLI

---
+++ codenvy-cli.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+VERSION=`basename assembly/target/codenvy-cli-*/`
+
+ASSEMBLY_BIN_DIR=assembly/target/$VERSION/$VERSION/bin
+
+if [ ! -d "${ASSEMBLY_BIN_DIR}" ]
+then
+  echo "$(tput setaf 1)The command 'mvn clean install' needs to be run first."$(tput sgr0)
+  exit 1
+fi
+
+cd $ASSEMBLY_BIN_DIR
+./codenvy-cli $*
