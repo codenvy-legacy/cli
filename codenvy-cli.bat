ca16b0b158c2f63a3540e41a2f8764d85c605e38
Florent Benoit <Florent.Benoit@serli.com>
2014-07-29 03:07:05 -0700

    fix #49 : do not change directory

--- codenvy-cli.bat
+++ codenvy-cli.bat
@@ -6,10 +6,7 @@ for /F %%i in ("%FILE%") do (set VERSION=%%~nxi)
 set ASSEMBLY_BIN_DIR=assembly\target\%VERSION%\%VERSION%\bin
 
 IF exist %ASSEMBLY_BIN_DIR% (
-    SET CDIR=%CD%
-    cd %ASSEMBLY_BIN_DIR%
-    call codenvy-cli.bat %1 %2 %3 %4 %5 %6 %7 %8
-    chdir %CDIR%
+    call %ASSEMBLY_BIN_DIR%\codenvy-cli.bat %1 %2 %3 %4 %5 %6 %7 %8
 ) else (
     echo The command 'mvn clean install' needs to be run first. This will build the CLI assembly
 )
