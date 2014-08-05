8f06cb1d917a1868cb1abcbef6db028cdc89dec3
Florent Benoit <Florent.Benoit@serli.com>
2014-08-05 13:59:47 +0200

    Fixes #13 destroy command

---
+++ command/src/main/java/com/codenvy/cli/command/builtin/DestroyCommand.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+
+package com.codenvy.cli.command.builtin;
+
+import com.codenvy.cli.command.builtin.model.UserBuilderStatus;
+import com.codenvy.cli.command.builtin.model.UserProject;
+import com.codenvy.cli.command.builtin.model.UserRunnerStatus;
+import com.codenvy.client.model.Link;
+import com.codenvy.client.model.Project;
+import com.codenvy.client.model.RunnerState;
+
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
+import org.fusesource.jansi.Ansi;
+
+import static com.codenvy.cli.command.builtin.MultiRemoteCodenvy.checkOnlyOne;
+import static org.fusesource.jansi.Ansi.Color.RED;
+
+/**
+ * Destroy command.
+ * This command will destroy a project
+ * @author Florent Benoit
+ */
+@Command(scope = "codenvy", name = "destroy", description = "Destroy a project")
+public class DestroyCommand extends AbsCommand {
+
+    @Argument(name = "project-id", description = "Specify the project ID to use", required = true, multiValued = false)
+    private String projectId;
+
+
+    /**
+     * Prints the current projects per workspace
+     */
+    protected Object doExecute() {
+        init();
+
+        // not logged in
+        if (!checkifEnabledRemotes()) {
+            return null;
+        }
+
+        // do we have the projectID ?
+        if (projectId == null) {
+            Ansi buffer = Ansi.ansi();
+            buffer.fg(RED);
+            buffer.a("No projectID has been set");
+            buffer.reset();
+            System.out.println(buffer.toString());
+            return null;
+        }
+
+        // get project for the given shortID
+        UserProject project = getMultiRemoteCodenvy().getProject(projectId);
+
+        if (project == null) {
+            Ansi buffer = Ansi.ansi();
+            buffer.fg(RED);
+            buffer.a("No matching project for identifier '").a(projectId).a("'.");
+            buffer.reset();
+            System.out.println(buffer.toString());
+            return null;
+        }
+
+        final Project projectToDestroy = project.getInnerProject();
+
+        project.getCodenvy().project().deleteResources(projectToDestroy, "").execute();
+        System.out.println(String.format("The project %s has been destroyed", projectToDestroy.name()));
+
+
+        return null;
+    }
+
+
+
+
+
+}
